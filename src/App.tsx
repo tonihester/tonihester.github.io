da2b3cde397db25c66b8f12f92726ea559271065
Simon Stassen <tonihester@emailsimon.net>
2023-12-28 10:34:07 +1100

    customize

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import background from './annie-spratt-wood-horizontal.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={background} className="hero-logo" alt="wood hero image" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Tony Hester
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
